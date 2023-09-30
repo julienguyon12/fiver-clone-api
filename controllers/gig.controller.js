4ed31c27fbb9f89227d0abb84dd64f7f42814741
julienguyon12 <julien.guyon@2018.icam.fr>
2023-09-30 13:56:13 +0200

    weekly update: create database and server user,gig endpoint done

---
+++ controllers/gig.controller.js
@@ -0,0 +1,58 @@
+import Gig from "../models/gig.model.js";
+import createError from "../utility/createError.js";
+
+export const createGig = async (req, res, next) => {
+  if (!req.isSeller)
+    return next(createError(403, "Only sellers can create a gig"));
+  const newGig = new Gig({
+    userId: req.userId,
+    ...req.body,
+  });
+  try {
+    const saveGig = await newGig.save();
+    res.status(201).json(saveGig);
+  } catch (err) {
+    next(err);
+  }
+};
+export const deleteGig = async (req, res, next) => {
+  try {
+    const gig = await Gig.findById(req.params.id);
+
+    if (gig.userId !== req.userId)
+      return next(createError(403, "You can only delete your gig"));
+    await Gig.findByIdAndDelete(req.params.id);
+    res.status(200).send("Gig has been deleted");
+  } catch (err) {
+    next(err);
+  }
+};
+export const getGig = async (req, res, next) => {
+  try {
+    const gig = await Gig.findById(req.params.id);
+    if (!gig) return next(createError(404, "This gig doesn't exist"));
+    res.status(200).send(gig);
+  } catch (err) {
+    next(err);
+  }
+};
+export const getGigs = async (req, res, next) => {
+  const query = req.query;
+  const filters = {
+    ...(query.userId && { userId: query.userId }),
+    ...(query.cat && { cat: query.cat }),
+    ...((query.min || query.max) && {
+      price: {
+        ...(query.min && { $gt: query.min }),
+        ...(query.max && { $lt: query.max }),
+      },
+    }),
+    ...(query.search && { title: { $regex: query.search, $options: "i" } }),
+  };
+  try {
+    const gigs = await Gig.find(filters).sort({ [query.sort]: -1 });
+    res.status(200).send(gigs);
+  } catch (err) {
+    next(err);
+  }
+};
---
+++ controllers/user.controller.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
+import createError from "../utility/createError.js";
+
+export const deleteUser = async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+
+  if (req.userId !== user._id.toString()) {
+    return next(createError(403, "You can delete only your account"));
+  }
+  await User.findByIdAndDelete(req.params.id);
+  res.status(200).send("user deleted");
+};
+
+export const getUser = async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+
+  res.status(200).send(user);
+};
